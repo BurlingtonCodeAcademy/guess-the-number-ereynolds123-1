21d4b6bed5a672d0e1e88f895c461c69079f89b2
Emily Reynolds <emilyreynoldsdsa@gmail.comgit config --global user.name Emily>
2020-06-03 21:00:25 -0400

    random integer functionality.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Readline boiler plate code, allows for asynchronous programmin
+//Readline boiler plate code, allows for asynchronous programming
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
@@ -9,15 +9,19 @@ function ask(questionText) {
 }
 
 //Variable declarations 
-let randomInteger= randomInt(100);
+//let randomInteger= randomInt(100);
+let min= 1;
+let max= 100;
+let randomInteger= randomNum(min, max);
+
 
 
 //Function starts the game
 start();
 
-//Computer randomly generats an integer between 1-100. Stores variable
- function randomInt(max){
-  let computerGuess= Math.floor(Math.random()*Math.floor(max));
+//Function that gives random number in a range
+function randomNum(min, max) {
+  let computerGuess= Math.floor(Math.random() * (max - min + 1) + min);
   return computerGuess;
 }
 
@@ -27,18 +31,33 @@ async function start() {
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
   console.log("The computer guessed: "+ randomInteger);
-  let input= await ask ("Is that your secret number? Type yes or no.")
+  let input = await ask ("Is that your secret number? Type yes (y) or no (n).\n")
   
+//loop here
+
   //If input is secret number, displays victory message and exits. If not, awaits second guess. If nonesense input, asks again. 
-  if (input==="yes"){
-    console.log("Congratulations! You won!");
+  if (input==="yes"||input==="y"){
+    //Displays victory message
+    console.log("Congratulations! The computer won!");
     process.exit()
-  }/*else if (input==="no"){
-    ask ("Is the number higher (type 'H') or lower (type 'L')?")
-  }*/ //This code isn't working
+  }else if (input==="no"|| input==="n" ){
+  /*let higherLower= or loop*/  ask ("Is the number higher (type 'H') or lower (type 'L')?")
+  }
   else{
     console.log ("Type yes or no!");
   }
 
   process.exit();
 }
+
+/*let higherLower= await ask ("Is ")
+  if (higherLower==="h"){
+  min=randomInteger;
+} else if(higherLower==="l){
+  max=randomInteger*/
+
+
+/*while (input !=="y"|| input !=="yes"){
+  make a guess
+  ask if the guess is correct
+  if not ask higher or lower*/
